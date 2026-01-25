b8669c12ee503cd9330ef579fa1dce507a29546e
Pubgggdie <91722397+Pubgggdie@users.noreply.github.com>
2026-01-25 16:06:12 +0530

    style: adjust main container spacing to space-y-8

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import About from "@/components/about";
 
 export default function Home() {
   return (
-    <main className="min-h-screen max-w-3xl mx-auto py-12 md:py-16 space-y-10 border border-dashed bg-background">
+    <main className="min-h-screen max-w-3xl mx-auto py-12 md:py-16 space-y-8 border border-dashed bg-background">
       <Hero />
       <Skills />
       <About />
