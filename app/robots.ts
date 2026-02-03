4408fc339ea6951b87d4d7f6c08f6d5a37a2d951
Pubgggdie <91722397+Pubgggdie@users.noreply.github.com>
2026-02-03 20:52:07 +0530

    ROBOT.TS CHANGES

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: "https://root.akoder.xyz/sitemap.xml",
+    sitemap: "https://akoder.xyz/sitemap.xml",
   };
 }
